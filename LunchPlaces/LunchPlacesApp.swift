6eb3b8871d119b73ffbc8778a3f71d71c1afecce
Jeremy Greenwood <jagreenwood@gmail.com>
2022-05-05 15:39:03 -0400

    project init

---
+++ LunchPlaces/LunchPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchPlacesApp.swift
+//  LunchPlaces
+//
+//  Created by Jeremy Greenwood on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct LunchPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
