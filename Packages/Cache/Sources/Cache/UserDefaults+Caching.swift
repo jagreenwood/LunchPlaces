69185e0e9f254fa2af2e8624b6ff7e1929dbb031
Jeremy Greenwood <jagreenwood@gmail.com>
2022-05-06 07:19:52 -0400

    add user defaults conformance

---
+++ Packages/Cache/Sources/Cache/UserDefaults+Caching.swift
@@ -0,0 +1,20 @@
+//
+//  UserDefaults+Caching.swift
+//  
+//
+//  Created by Jeremy Greenwood on 5/6/22.
+//
+
+import Foundation
+
+extension UserDefaults: Caching {
+    public func cache<T>(_ object: T, key: String) throws where T : Decodable, T : Encodable {
+        let data = try JSONEncoder().encode(object)
+        set(data, forKey: key)
+    }
+
+    public func object<T>(key: String) throws -> T? where T : Decodable, T : Encodable {
+        guard let data = data(forKey: key) else { return nil }
+        return try JSONDecoder().decode(T.self, from: data)
+    }
+}
