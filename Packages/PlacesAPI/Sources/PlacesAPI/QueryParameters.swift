be69ad2b3391aee7f83921f4997ee70fa800f32f
Jeremy Greenwood <jagreenwood@gmail.com>
2022-05-07 13:05:41 -0400

    places endpoints definition

---
+++ Packages/PlacesAPI/Sources/PlacesAPI/QueryParameters.swift
@@ -0,0 +1,37 @@
+//
+//  QueryParameters.swift
+//  
+//
+//  Created by Jeremy Greenwood on 5/7/22.
+//
+
+import Foundation
+
+public struct QueryParameters {
+    let location: (lat: Double, long: Double)
+    let pageToken: String?
+    let radius: Double
+    let type: String
+
+    public init(
+        location: (lat: Double, long: Double),
+        pageToken: String,
+        radius: Double,
+        type: String) {
+            self.location = location
+            self.pageToken = pageToken
+            self.radius = radius
+            self.type = type
+        }
+}
+
+extension QueryParameters {
+    var dictionaryRepresentation: [String: String] {
+        ["latitude": String(location.lat),
+         "longitude": String(location.long),
+         "pagetoken": pageToken,
+         "radius": String(radius),
+         "type": type]
+            .compactMapValues { $0 }
+    }
+}
