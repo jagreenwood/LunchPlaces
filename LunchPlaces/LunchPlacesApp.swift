10cc4a1d4716ec97727767426d17605d1d22713b
Jeremy Greenwood <jagreenwood@gmail.com>
2022-05-08 06:30:44 -0400

    App Feature (#26)
    * stub app domain
    
    * show app view as root view of app

--- LunchPlaces/LunchPlacesApp.swift
+++ LunchPlaces/LunchPlacesApp.swift
@@ -5,13 +5,19 @@
 //  Created by Jeremy Greenwood on 5/5/22.
 //
 
+import App
 import SwiftUI
 
 @main
 struct LunchPlacesApp: App {
+    let store = Store(
+        initialState: .init(),
+        reducer: AppDomain.reducer,
+        environment: .mock(.mock))
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppView(store: store)
         }
     }
 }
---
+++ Packages/Features/Sources/App/AppView.swift
@@ -0,0 +1,40 @@
+//
+//  AppView.swift
+//
+//
+//  Created by Jeremy Greenwood on 05/07/2022.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+public struct AppView: View {
+    let store: Store<AppDomain.State, AppDomain.Action>
+
+    public init(store: Store<AppDomain.State, AppDomain.Action>) {
+        self.store = store
+    }
+
+    public var body: some View {
+        WithViewStore(store) { viewStore in
+            Text(viewStore.name)
+                .padding()
+                .onAppear {
+                    viewStore.send(.onAppear)
+                }
+        }
+    }
+}
+
+#if DEBUG
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView(
+            store: Store(
+                initialState: AppDomain.State(),
+                reducer: AppDomain.reducer,
+                environment: .mock(.mock))
+        )
+    }
+}
+#endif
