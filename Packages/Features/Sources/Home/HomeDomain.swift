82d5b9a07aec7890fec6b3961c91edc147452ef0
Jeremy Greenwood <jagreenwood@gmail.com>
2022-05-10 09:05:13 -0400

    stub actions and state

--- Packages/Features/Sources/Home/HomeDomain.swift
+++ Packages/Features/Sources/Home/HomeDomain.swift
@@ -9,10 +9,18 @@ import Common
 import ComposableArchitecture
 import Foundation
 import LocationService
+import Model
 
 public struct HomeDomain: Equatable {
+    enum Route: Equatable {
+        case placeDetail(Place)
+    }
+
     public struct State: Equatable {
+        @BindableState var route: Route?
         var locationServiceState = LocationServiceDomain.State()
+        var places: [Place] = []
+        var showMap = false
 
         public var name: String
 
@@ -21,9 +29,14 @@ public struct HomeDomain: Equatable {
         }
     }
 
-    public enum Action: Equatable {
+    public enum Action: BindableAction, Equatable {
+        case binding(BindingAction<State>)
+        case fetchLocation
+        case fetchRestaurants
         case locationService(LocationServiceDomain.Action)
         case onAppear
+        case setRestaurants([Place])
+        case toggleMap
     }
 
     public struct Environment {
@@ -46,13 +59,30 @@ public struct HomeDomain: Equatable {
                 environment: { $0.locationServiceEnvironment }),
         Reducer { state, action, _ in
             switch action {
+            case .binding:
+                return .none
+
+            case .fetchLocation:
+                return .none
+
+            case .fetchRestaurants:
+                return .none
+
             case .locationService:
                 return .none
 
             case .onAppear:
                 state.name = "Home"
                 return .none
+
+            case .setRestaurants(let places):
+                state.places = places
+                return .none
+
+            case .toggleMap:
+                state.showMap.toggle()
+                return .none
             }
-        }
+        }.binding()
     )
 }
